c00fb344f0fee7e8a688014c5a9433093d186e17
Kain-1 <kainplath@gmail.com>
2021-09-27 20:04:54 -0700

    Module2Lab1

--- Week2/Module2Lab0/src/Interactive.java
+++ Week2/Module2Lab0/src/Interactive.java
@@ -29,22 +29,25 @@ public class Interactive {
                                 """;
 
                 System.out.println("Please Enter Your Initials: "); // prompt user
-                System.out.println("First Initial: ");
+                System.out.print("First Name/Initial: ");
                 Scanner input = new Scanner(System.in); // Make new object that reads user input from CLI
                 String initial1 = input.next(); // get initial 1
 
-                System.out.println("Second Initial: ");
+                System.out.print("Middle Name/Initial: ");
                 String initial2 = input.next(); // get initial 2
 
-                System.out.println("Third Initial: ");
+                System.out.print("Last Name/Initial: ");
                 String initial3 = input.next(); // get initial 3
                 input.close(); // stop getting input
 
-                String[] initialsArray = { initial1, initial2, initial3 }; // make array with each initial
+                String[] initialsArray = { initial1.substring(0, 1), initial2.substring(0, 1),
+                                initial3.substring(0, 1) }; // make array with each initial from first line of users
+                                                            // input
 
                 System.out.print("Your Initials: ");
                 for (String counter : initialsArray) {
                         System.out.print(counter.toUpperCase()); // print users initials
+                        System.out.print(".");
                 }
                 System.out.println("");
 
@@ -52,13 +55,8 @@ public class Interactive {
                                 && initialsArray[2].equalsIgnoreCase("r")) { // if initials = SLR
                         System.out.println("Initials as Stars: ");
                         System.out.println(SLR); // print SLR in stars
-                }
-                if (initialsArray[0].equalsIgnoreCase("b") && initialsArray[1].equalsIgnoreCase("r")
-                                && initialsArray[2].equalsIgnoreCase("g")) { // if initials = BRG
-                        System.out.println("Initials as Stars: ");
-                        System.out.println(BRG); // print BRG in stars
                 } else {
-                        System.out.println("Initials Not in Database."); // if none of the above
+                        System.out.println("Initials Not in Database."); // if not
                         System.out.println("Showing Default Combination:");
                         System.out.println(BRG); // print default initials
                 }
---
+++ Week2/Module2Lab1/src/Date.java
@@ -0,0 +1,75 @@
+import java.time.LocalDateTime; 
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+    public static void main(String[] args){
+        LocalDateTime now = LocalDateTime.now(); //Get Current Date/Time and assign it to now
+        //Output format: yyyy-MM-dd "T" HH:mm:ss.ms
+        //2021-09-27    T18:57:26.604537847
+
+        DateTimeFormatter DayFormatter = DateTimeFormatter.ofPattern("E");      //DayFormatter will extract day of the week from now
+        DateTimeFormatter YearFormatter = DateTimeFormatter.ofPattern("yyyy");  //YearFormatter will extract current year from now
+        DateTimeFormatter DateFormatter = DateTimeFormatter.ofPattern("dd");    //DateFormatter will extract current day of month from now
+        DateTimeFormatter MonthFormatter = DateTimeFormatter.ofPattern("MMM");  //MonthFormatter will extract current month in year from now
+ 
+        String day = now.format(DayFormatter);      //Use DayFormatter to set current day to day
+        String year = now.format (YearFormatter);   //Use YearFormatter to set current year to year
+        String date = now.format(DateFormatter);    //Use DateFormatter to set current day of month to date
+        String month = now.format(MonthFormatter);  //Use MonthFormatter to set current month in year to month
+
+        //Expands day format to include whole day name
+        //Since DayFormatter only returns the first 3 letters of the current day of week.
+        if (day.equals("Sun")){
+            day = "Sunday";
+        }if (day.equals("Mon")){
+            day = "Monday";
+        }if (day.equals("Tue")){
+            day = "Tuesday";
+        }if (day.equals("Wed")){
+            day = "Wednesday";
+        }if (day.equals("Thu")){
+            day = "Thursday";
+        }if (day.equals("Fri")){
+            day = "Friday";
+        } if(day.equals("Sat")){
+            day = "Saturday";
+        }
+
+        //Expands month format to include whole month name
+        //Since MonthFormatter only returns the first 3 letters of the current month in year
+        if (month.equals("Jan")){
+            month = "January";
+        }if (month.equals("Feb")){
+            month = "February";
+        }if (month.equals("Mar")){
+            month = "March";
+        }if (month.equals("Apr")){
+            month = "April";
+        }if (month.equals("May")){
+            month = "May";
+        }if (month.equals("Jun")){
+            month = "June";
+        }if (month.equals("Jul")){
+            month = "July";
+        }if (month.equals("Aug")){
+            month = "August";
+        }if (month.equals("Sep")){
+            month = "September";
+        }if (month.equals("Oct")){
+            month = "October";
+        }if (month.equals("Nov")){
+            month = "November";
+        }if (month.equals("Dec")){
+            month = "December";
+        }
+        
+        System.out.println();
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + ", " + year); //Print American Format
+
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year); //Print European Format
+        System.out.println();
+
+    }
+}
