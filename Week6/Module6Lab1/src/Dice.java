18c44293507b9e620d900147b21ad05b2b116ce3
Kain-1 <kainplath@gmail.com>
2021-10-27 22:05:44 -0700

    Created Module6Lab1/

--- Week6/Module6Lab1/src/Dice.java
+++ Week6/Module6Lab1/src/Dice.java
@@ -4,9 +4,13 @@ public class Dice {
     public static void main(String[] args) {
         Random random = new Random();
         for (int i = 1; i > 0; i++) {
+            //roll die 1
             int die1 = random.nextInt(6) + 1;
+            //roll die 2
             int die2 = random.nextInt(6) + 1;
+            //print results
             System.err.printf("Roll %d: %d & %d\n", i, die1, die2);
+            //if snake eyes, kill and tell user how many rolls it took.
             if (die1 == 1 && die2 == 1) {
                 System.out.printf("Snake Eyes! (Took %d rolls)\n", i);
                 break;
