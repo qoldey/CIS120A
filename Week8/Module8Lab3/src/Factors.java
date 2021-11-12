cd8f85c3b058bcccbd482da9ea2c7777e521d700
Kain-1 <kainplath@gmail.com>
2021-11-12 12:50:42 -0800

    Created Module8Lab3/

--- Week8/Module8Lab3/src/Factors.java
+++ Week8/Module8Lab3/src/Factors.java
@@ -1,15 +1,19 @@
 public class Factors {
     public static void main(String[] args) throws Exception {
 
-        int[] m = { 1, 3, 9, 11, 33, 99 };
-
+        // number to check
         int n = 99;
+        // test array
+        int[] m = { 1, 3, 9, 11, 33, 99 };
 
         System.out.println(areFactors(n, m));
     }
 
+    // a method named areFactors that takes an integer n and an array of integers,
+    // and that returns true if the numbers in the array are all factors of n
     public static boolean areFactors(int n, int[] m) {
 
+        // traverse m, if any number is not a factor, return false, else return true.
         for (int i = 0; i < m.length; i++) {
             if (!(n % m[i] == 0)) {
                 return false;
